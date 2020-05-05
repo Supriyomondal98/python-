b2df4af9e6765da67e68e9531eb374bd46dc074e
Supriyomondal98 <64784942+Supriyomondal98@users.noreply.github.com>
2020-05-05 17:46:37 +0530

    Add files via upload

---
+++ assingment1 task 2 .py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+n=int(input("enter number for pattern"))
+for i in range(n):
+    print("* "*(i+1))
+for i in range(n-1):
+    print("* "*(n-i-1))
+
+
+# In[2]:
+
+
+a=input("input-")
+print(a[::-1])
+
+
+# In[13]:
+
+
+n= (input("insert numbers seperated by comma"))
+n=[n]
+n
+
